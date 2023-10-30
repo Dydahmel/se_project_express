0712ed6e8365b6a03c0d9c2380f8ea61f5044c03
Anton Dutchak <106831892+Dydahmel@users.noreply.github.com>
2023-10-30 11:56:41 -0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 
-const { PORT = 3001, BASE_PATH } = process.env;
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+const { PORT = 3001 } = process.env;
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db',
+(r) => {
+  console.log ('Connected to MongoDB!!!', r)
+  });
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', require('/routes/users'))
 
 
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
+
+
+// database start
+//"C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath="c:\data\db"
+//server start
+//npm run start
